fa6b5e48a266587831e5579bdea3593820acec7c
holly.fromthe.nexus <iseeicyhawkston@gmail.com>
2023-09-26 17:01:05 -0500

    Sticky controls work

--- net.bobbo.text-window/projected_control.gd
+++ net.bobbo.text-window/projected_control.gd
@@ -13,8 +13,10 @@ class_name ProjectedControl
 #	Variables
 #
 
+const MARGIN = 8	# Used when offsetting off-screen control
 @onready var _camera = get_viewport().get_camera_3d()
 var _focus_target = null 
+var _distance_to_target: float = 0
 
 #
 #	Godot Functions
@@ -34,17 +36,23 @@ func _process(delta):
 func _process_while_targeting(delta: float) -> void:
 	var target_transform = get_focus_target().global_transform
 	
-	var is_behind = _is_transform_behind_cam(target_transform, _camera)
-	var distance = _distance_to_cam(target_transform, _camera)
+	_distance_to_target = _distance_to_cam(target_transform, _camera)
 	var unprojected_position = _camera.unproject_position(target_transform.origin)
-	var viewport_base_size = _get_viewport_base_size()
 	
 	# For non-sticky controls, we have all we need. If the target
 	# is off screen we'll just hide this node.
 	if not is_sticky:
 		position = unprojected_position
-		visible = not is_behind
-		return
+		visible = not _is_transform_behind_cam(target_transform, _camera)
+	# For sticky controls, we have to correct the unprojected
+	# position in order to address the edges of the screen
+	else:
+		position = _correct_unprojected_position(
+			target_transform, 
+			_camera, 
+			unprojected_position
+		)
+		
 
 	
 	# Fade the waypoint when the camera gets close.
@@ -87,3 +95,37 @@ func _get_viewport_base_size():
 		return get_viewport().content_scale_size
 	else:
 		return get_viewport().size
+		
+func _correct_unprojected_position(target_transform, cam, unprojected_position):
+	var is_behind = _is_transform_behind_cam(target_transform, cam)
+	var viewport_base_size = _get_viewport_base_size()
+	# We need to handle the axes differently.
+		
+	# For the screen's X axis, the projected position is useful to us,
+	# but we need to force it to the side if it's also behind.
+	if is_behind:
+		if unprojected_position.x < viewport_base_size.x / 2:
+			unprojected_position.x = viewport_base_size.x - MARGIN
+		else:
+			unprojected_position.x = MARGIN
+			
+	# For the screen's Y axis, the projected position is NOT useful to us
+	# because we don't want to indicate to the user that they need to look
+	# up or down to see something behind them. Instead, here we approximate
+	# the correct position using difference of the X axis Euler angles
+	# (up/down rotation) and the ratio of that with the camera's FOV.
+	# This will be slightly off from the theoretical "ideal" position.
+	if is_behind or unprojected_position.x < MARGIN or \
+			unprojected_position.x > viewport_base_size.x - MARGIN:
+		var look = cam.global_transform.looking_at(target_transform.origin, Vector3.UP)
+		var diff = _calc_angle_diff(look.basis.get_euler().x, cam.global_transform.basis.get_euler().x)
+		unprojected_position.y = viewport_base_size.y * (0.5 + (diff / deg_to_rad(cam.fov)))
+
+	return Vector2(
+			clamp(unprojected_position.x, MARGIN, viewport_base_size.x - MARGIN),
+			clamp(unprojected_position.y, MARGIN, viewport_base_size.y - MARGIN)
+	)
+
+func _calc_angle_diff(from, to):
+	var diff = fmod(to - from, TAU)
+	return fmod(2.0 * diff, TAU) - diff
