7c82288e83c2abebd7bf683691152c010af008d1
holly.fromthe.nexus <iseeicyhawkston@gmail.com>
2023-09-28 16:07:18 -0500

    Imported state machine

---
+++ net.bobbo.state-machine/plugin.gd
@@ -0,0 +1,20 @@
+@tool
+extends EditorPlugin
+
+func _enter_tree():
+	add_custom_type(
+		"BobboState",					# Type name
+		"Node", 					# Base type name
+		preload("bobbo_state.gd"), 		# Script for type
+		preload("icons/bobbo_state.png")	# Icon for type
+	)
+	add_custom_type(
+		"BobboStateMachine",					# Type name
+		"Node",								# Base type name
+		preload("bobbo_state_machine.gd"), 		# Script for type
+		preload("icons/bobbo_state_machine.png")	# Icon for type
+	)
+
+func _exit_tree():
+	remove_custom_type("BobboState")
+	remove_custom_type("BobboStateMachine")
--- net.bobbo.text-window/text_window.gd
+++ net.bobbo.text-window/text_window.gd
@@ -1,4 +1,40 @@
 extends Control
 class_name TextWindow
 
+#
+#	Exported
+#
 
+signal speaker_changed(new_speaker: String)
+signal reader_settings_changed(new_reader_settings: TextReaderSettings)
+
+#
+#	Variables
+#
+
+var _speaker: String = ""
+var _reader_settings: TextReaderSettings = null
+
+#
+#	Functions
+#
+
+func set_speaker(new_speaker: String) -> void:
+	_speaker = new_speaker
+	speaker_changed.emit(new_speaker)
+
+func get_speaker() -> String:
+	return _speaker
+
+func set_reader_settings(settings: TextReaderSettings) -> void:
+	_reader_settings = settings
+	reader_settings_changed.emit(settings)
+
+func get_reader_settings() -> TextReaderSettings:
+	return _reader_settings
+
+func show_text(new_text: String) -> void:
+	return
+
+func show_choices(choices: Array[String]) -> void:
+	return
