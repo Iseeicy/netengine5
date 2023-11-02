a021863e9295e1f2d87c8d5cda7a9ac32b0dc836
holly.fromthe.nexus <iseeicyhawkston@gmail.com>
2023-11-02 15:11:42 -0500

    Dialog Graph Editor has preview window

--- net.bobbo.dialog-graph/dialog_preview.gd
+++ net.bobbo.dialog-graph/dialog_preview.gd
@@ -3,21 +3,46 @@ extends Control
 class_name DialogPreview
 
 #
+#	Exports
+#
+
+@export var graph_editor: DialogGraphEditor
+
+#
 #	Private Variables
 #
 
-var _text_window: TextWindow
+@onready var _dialog_runner: DialogRunner = $DialogRunner
+var _selected_nodes: Array[DialogGraphNode] = []
+var _temp_graph: DialogGraph = DialogGraph.new()
 
 #
-#	Godot Functions
+#	Private Functions
 #
 
-func _ready():
-	_text_window = $DialogPreviewTextWindow
+func _update_selected_nodes() -> void:
+	# Update the internal version of our graph
+	graph_editor.save_to_resource(_temp_graph)
+	
+	if _selected_nodes.size() > 0:
+		var data = _selected_nodes[-1].get_node_data()
+		var node_id = _temp_graph.find_id_from_data(data)
+		
+		if node_id != -1:
+			_dialog_runner.run_dialog(_temp_graph, node_id)
+	else:
+		_dialog_runner.stop_dialog()
 
 #
-#	Public Functions
+#	Signals
 #
 
-func get_text_window() -> TextWindow:
-	return _text_window
+func _on_dialog_graph_editor_node_selected(node: DialogGraphNode):
+	_selected_nodes.push_back(node)
+	_update_selected_nodes()
+
+func _on_dialog_graph_editor_node_deselected(node: DialogGraphNode):
+	_selected_nodes.erase(node)
+	_update_selected_nodes()
+
+
