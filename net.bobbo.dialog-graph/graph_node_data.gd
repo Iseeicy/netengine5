4ac6a05eac871d71b20c9a2551f4f51a6cf31428
holly.fromthe.nexus <iseeicyhawkston@gmail.com>
2023-11-06 12:56:29 -0600

    Documented node data

--- net.bobbo.dialog-graph/graph_node_data.gd
+++ net.bobbo.dialog-graph/graph_node_data.gd
@@ -1,3 +1,5 @@
+## Represents basic data relating to a GraphNode. Meant to be extended.
+@tool
 extends Resource
 class_name GraphNodeData
 
@@ -5,4 +7,5 @@ class_name GraphNodeData
 #	Exports
 #
 
+## The position of this node in the graph that it belongs to.
 @export var position: Vector2 = Vector2.ZERO
