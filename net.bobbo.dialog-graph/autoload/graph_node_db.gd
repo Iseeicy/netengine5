a9227b2ae804cba0cf32bdb49a21ad010bc2b9a2
holly.fromthe.nexus <iseeicyhawkston@gmail.com>
2023-11-06 13:10:55 -0600

    Documented GraphNodeDB

--- net.bobbo.dialog-graph/autoload/graph_node_db.gd
+++ net.bobbo.dialog-graph/autoload/graph_node_db.gd
@@ -1,3 +1,5 @@
+## An autoload class that acts as reference for what kinds of dialog nodes
+## exist, and how to work with them.
 @tool
 extends Node
 
@@ -5,13 +7,19 @@ extends Node
 #	Exports
 #
 
-@export var descriptors: Array[DialogGraphNodeDescriptor] = []
+## A list of descriptors that represent what kind of dialog nodes exist.
+@export var descriptors: Array[DialogGraphNodeDescriptor] = [
+	preload("../nodes/entry/entry_desc.tres"),
+	preload("../nodes/dialog_text/dialog_text_desc.tres"),
+	preload("../nodes/choice_prompt/choice_prompt_desc.tres"),
+	preload("../nodes/forwarder/forwarder_desc.tres"),
+]
 
 #
 #	Public Funcitons
 #
 
-## Given some node data, find the descriptor that it belongs to
+## Given some node data, find the descriptor that it belongs to.
 func find_descriptor_for_data(data: GraphNodeData) -> DialogGraphNodeDescriptor:
 	for desc in descriptors:
 		if desc.data_script == data.get_script():
