3d24571bc703de5a2714bcde1c7ec2c74eb0bf62
holly.fromthe.nexus <iseeicyhawkston@gmail.com>
2023-11-06 14:07:23 -0600

    Documented DialogPreview

--- net.bobbo.dialog-graph/editor/preview_text_window/dialog_preview.gd
+++ net.bobbo.dialog-graph/editor/preview_text_window/dialog_preview.gd
@@ -1,3 +1,4 @@
+## A control used to preview the current state of the DialogGraphEditor.
 @tool
 extends VBoxContainer
 class_name DialogPreview
@@ -13,14 +14,21 @@ signal request_graph_update()
 #	Private Variables
 #
 
+## The DialogRunner that we'll use to execute the current DialogGraph
 @onready var _dialog_runner: DialogRunner = $DialogRunner
+## A list of nodes that are currently selected. We keep track of this so that
+## we can figure out what the last selected node was.
 var _selected_nodes: Array[DialogGraphNode] = []
+## A reference to the internal DialogGraph cache to use and run through with
+## this preview.
 var _temp_graph: DialogGraph = DialogGraph.new()
 
 #
 #	Public Functions
 #
 
+## Update the graph that this preview is using, and re-start executing it
+## from the last selected node if there is one.
 func update_graph(new_graph: DialogGraph) -> void:
 	_temp_graph = new_graph
 	
@@ -37,15 +45,13 @@ func update_graph(new_graph: DialogGraph) -> void:
 #	Signals
 #
 
+## Called when the DialogGraphEditor selects a node in the graph.
 func _on_dialog_graph_editor_node_selected(node: DialogGraphNode):
-	print("selected ", node)
 	_selected_nodes.push_back(node)
 	request_graph_update.emit()
 
+## Called when the DialogGraphEditor deselects a previously selected node in
+## the graph.
 func _on_dialog_graph_editor_node_deselected(node: DialogGraphNode):
-	print("deselected ", node)
-	
 	_selected_nodes.erase(node)
 	request_graph_update.emit()
-
-
