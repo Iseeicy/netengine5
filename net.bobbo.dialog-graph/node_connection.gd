f19fec4b235c80305f743ce756a254fac39ecf5b
holly.fromthe.nexus <iseeicyhawkston@gmail.com>
2023-11-06 12:48:46 -0600

    Documented node connection

--- net.bobbo.dialog-graph/node_connection.gd
+++ net.bobbo.dialog-graph/node_connection.gd
@@ -1,3 +1,4 @@
+## Represents a connection between two nodes in an easily serializable way.
 extends Resource
 class_name NodeConnection
 
@@ -5,15 +6,23 @@ class_name NodeConnection
 #	Public Variables
 #
 
+## The ID of the node that this connection starts from
 @export var from_id: int = -1
+
+## The port on the node that this connection starts from
 @export var from_port: int = 0
+
+## The ID of the node that this connection goes to
 @export var to_id: int = -1
+
+## The port on the node that this connection goes to
 @export var to_port: int = 0
 
 #
 #	Public Functions
 #
 
+## Copy the properties of this connection to another connection.
 func copy_to(other: NodeConnection) -> void:
 	other.from_id = self.from_id
 	other.from_port = self.from_port
@@ -24,6 +33,7 @@ func copy_to(other: NodeConnection) -> void:
 #	Static Functions
 #
 
+## Create a new connecion object in memory given details about the connection
 static func create(node_from_id: int, node_from_port: int, node_to_id: int, node_to_port: int) -> NodeConnection:
 	var connection = NodeConnection.new()
 	connection.from_id = node_from_id
