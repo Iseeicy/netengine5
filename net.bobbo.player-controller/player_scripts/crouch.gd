379c63524fc040cc39bad2efd4a066735fbcbe17
ButterAleks <42917647+ButterAleks@users.noreply.github.com>
2023-11-21 20:00:11 -0800

    Updated crouch.gd to match requests
    This involved changing some variable names, changes how things worked so that instead of calculating new player height it's more boolean so the player stays at the same height of a normal crouch instead, and changed the formatting and style of the code a bit to make it cleaner

--- net.bobbo.player-controller/player_scripts/crouch.gd
+++ net.bobbo.player-controller/player_scripts/crouch.gd
@@ -14,23 +14,24 @@ var crouch_current: float = 0
 var crouch_target: float = 0
 const crouch_action: String = "player_crouch"
 
-## max_player_height is needed for proper uncrouching height calculations if the maximum player height were to change during gameplay at any point this value would need to somehow be updated to account for that default is 2 because the default height in player_controller is 2
-var _max_player_height: float = 2
-
-## This value reports the total height above the player's origin, it's also used to clamp what the player's height can be set to
-var _height_above: float = INF
+## max_player_height is needed for proper uncrouching height calculations if the maximum player 
+# height were to change during gameplay at any point this value would need to somehow be updated to 
+# account for that default is 2 because the default height in player_controller is 2
+var _default_player_height: float = 2
+var _should_be_crouched: bool = false
 
 #
 #	Functions
 #
 
 func player_ready():
-	_max_player_height = player.height
+	_default_player_height = player.height
 	self.assert_input_action(crouch_action)
 
 func player_physics_process(delta: float):
-	if Input.is_action_pressed(crouch_action):
+	if Input.is_action_pressed(crouch_action) or _should_force_crouch():
 		crouch_target = 0.5
+		_should_be_crouched = true
 	else:
 		crouch_target = 1
 		
@@ -41,36 +42,34 @@ func player_physics_process(delta: float):
 	
 	player.velocity.y += scale_difference * 0.5
 	
-	# Make it so that the crouch can only go as low as regularly crouching and only high enough such that the player doesn't get stuck if an object is detected above them
-	player.height = clamp(new_scale, _max_player_height / 2, _height_above)
-	
+	# Make it so that the crouch can only go as low as regularly crouching and only high enough such
+	# that the player doesn't get stuck if an object is detected above them
+	player.height = new_scale
+
+func _should_force_crouch():
 	# Get the 3D physics space from a Node3D like the player_pivot
 	var space_state = player.model_pivot.get_world_3d().direct_space_state
 	
+	var raycast_position_offset = 0
+	if(_should_be_crouched):
+		raycast_position_offset = _default_player_height / 4
+	
 	# Calculate origin and ending of raycast
-	var origin = player.position
-	var end = player.position + Vector3(0, _max_player_height / 2, 0)
+	var origin = player.position + player.up_direction * raycast_position_offset
+	var end = origin + player.up_direction * _default_player_height / 2
 	
 	# Create the raycast
 	var query = PhysicsRayQueryParameters3D.create(origin, end)
 	
-	# Exclude the player from raycast calculations and make the ray collide with areas
+	# Exclude the player from raycast calculations
 	query.exclude = [player]
-	query.collide_with_areas = true
 	
 	# Cast the raycast
-	var result = space_state.intersect_ray(query)
+	var ceiling_collision = space_state.intersect_ray(query)
 	
 	# If we detect an object at the top of the player
-	if result:
-		# Before this equation I subtracted by an offset of 0.001 so that way the height didn't exactly match so the player wouldn't get stuck, thankfully the equations I came up with seem to not need it
-		
-		# Calculate proper height to fit below object above the player
-		# Need to have a slightly different equation in case this happens to get proper crouch height
-		if(abs(player.position.y) > abs(result.position.y)):
-			_height_above = abs(player.position.y - result.position.y + (_max_player_height / 4)) + (_max_player_height / 2)
-		else:
-			_height_above = abs(result.position.y - player.position.y + (_max_player_height / 4))
+	if ceiling_collision:
+		return true
 	else:
-		# Unlock player height if there is no object detected at the top of the player
-		_height_above = INF
+		_should_be_crouched = false
+		return false
