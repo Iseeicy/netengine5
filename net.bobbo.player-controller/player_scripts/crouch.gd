44c1f247adfe3af03c5232caa808cf990c9bea75
ButterAleks <42917647+ButterAleks@users.noreply.github.com>
2023-11-28 14:28:25 -0800

    Fixed random crouching and updated code a bit
    Random crouching would happen when there was an obstruction above the player that matched their height, I found that this was due to a float precision error and so I just subtracted by a small offset to account for it. I also updated some comments to be more accurate and improved the code a bit based on the comments on the latest request at the time of writing

--- net.bobbo.player-controller/player_scripts/crouch.gd
+++ net.bobbo.player-controller/player_scripts/crouch.gd
@@ -14,11 +14,15 @@ var crouch_current: float = 0
 var crouch_target: float = 0
 const crouch_action: String = "player_crouch"
 
-## max_player_height is needed for proper uncrouching height calculations if the maximum player 
-# height were to change during gameplay at any point this value would need to somehow be updated to 
-# account for that default is 2 because the default height in player_controller is 2
+## default_player_height is needed for proper uncrouching height calculations 
+# if the maximum player height were to change during gameplay at any point this 
+# value would need to somehow be updated to account for that default is 2 
+# because the default height in player_controller is 2
 var _default_player_height: float = 2
 var _should_be_crouched: bool = false
+
+const _standing_height_multiplier: float = 1
+const _crouching_height_multiplier: float = 0.5
 
 #
 #	Functions
@@ -30,20 +34,17 @@ func player_ready():
 
 func player_physics_process(delta: float):
 	if Input.is_action_pressed(crouch_action) or _should_force_crouch():
-		crouch_target = 0.5
+		crouch_target = _crouching_height_multiplier
 		_should_be_crouched = true
 	else:
-		crouch_target = 1
-		
+		crouch_target = _standing_height_multiplier
+
 	var previous_scale = player.height * crouch_current
 	crouch_current = lerp(crouch_current, crouch_target, clamp(delta * crouch_speed, 0, 1))
 	var new_scale = player.height * crouch_current
 	var scale_difference = new_scale - previous_scale
 	
-	player.velocity.y += scale_difference * 0.5
-	
-	# Make it so that the crouch can only go as low as regularly crouching and only high enough such
-	# that the player doesn't get stuck if an object is detected above them
+	player.velocity.y += scale_difference * _crouching_height_multiplier
 	player.height = new_scale
 
 func _should_force_crouch():
@@ -52,11 +53,16 @@ func _should_force_crouch():
 	
 	var raycast_position_offset = 0
 	if(_should_be_crouched):
-		raycast_position_offset = _default_player_height / 4
+		raycast_position_offset = (_default_player_height / 2) * _crouching_height_multiplier
 	
 	# Calculate origin and ending of raycast
 	var origin = player.position + player.up_direction * raycast_position_offset
-	var end = origin + player.up_direction * _default_player_height / 2
+	
+	# We subract by an offset of 0.001 due to a float precision error that can 
+	# occur when an obstruction is just barely above the player causing the 
+	# raycast to barely detect it and making the player crouch so that doesn't
+	# happen
+	var end = origin + player.up_direction * (_default_player_height / 2 - 0.001)
 	
 	# Create the raycast
 	var query = PhysicsRayQueryParameters3D.create(origin, end)
