d796ff7ca36512e32c62e93dc2cceab0328c9576
holly.fromthe.nexus <iseeicyhawkston@gmail.com>
2024-03-24 11:38:44 -0500

    Separated 2D and 3D scenes

--- net.bobbo.item/item_instance.gd
+++ net.bobbo.item/item_instance.gd
@@ -10,7 +10,8 @@ class_name ItemInstance
 enum SpaceState {
 	NOWHERE = 0, # This item doesn't exist anywhere other than in memory!
 	IN_INVENTORY = 1, # This item is in an inventory somewhere.
-	IN_WORLD = 2, # This item is in the game world somewhere.
+	IN_WORLD_2D = 2, # This item is in the 2D game world somewhere.
+	IN_WORLD_3D = 3, # This item is in the 3D game world somewhere.
 }
 
 ## Used to indicate if an item instance operation succeeded, or if there was
@@ -43,8 +44,10 @@ signal stack_size_changed(size: int)
 var _descriptor: ItemDescriptor = null
 ## Generally where is this object spatially?
 var _space_state: SpaceState = SpaceState.NOWHERE
-## If this instance is IN_WORLD, then this is the WorldItem that represents us.
-var _current_world_item: Node = null
+## If this instance is IN_WORLD_2D, then this is the WorldItem that represents us.
+var _current_world_item_2d: WorldItem2D = null
+## If this instance is IN_WORLD_3D, then this is the WorldItem that represents us.
+var _current_world_item_3d: WorldItem3D = null
 ## If this instance is IN_INVENTORY, then this is the ItemInventory that the
 ## item is currently contained in.
 var _current_parent_inventory: ItemInventory = null
@@ -77,10 +80,15 @@ func get_descriptor() -> ItemDescriptor: return _descriptor
 ## Returns the current SpaceState of this item.
 func get_space_state() -> SpaceState: return _space_state
 
-## If this instance is in the game world, then this returns the object that
+## If this instance is in the 2D game world, then this returns the object that
 ## represents it. Otherwise, this returns null
-func get_world_item():
-	return _current_world_item if _space_state == SpaceState.IN_WORLD else null
+func get_world_item_2d() -> WorldItem2D:
+	return _current_world_item_2d if _space_state == SpaceState.IN_WORLD_2D else null
+
+## If this instance is in the 3D game world, then this returns the object that
+## represents it. Otherwise, this returns null
+func get_world_item_3d() -> WorldItem3D:
+	return _current_world_item_3d if _space_state == SpaceState.IN_WORLD_3D else null
 
 ## Returns how many items are represented by this instance. Typically for equipment
 ## this is 1, though for materials this number could increase.
@@ -137,45 +145,87 @@ func split_stack(new_stack_size: int) -> ItemInstance:
 	return new_item
 
 ## Spawn and return a new instance of this item's view model, if there is one. 
-## Returns null if there isn't one. Otherwise, returns an ItemViewModel2D or
-## ItemViewModel3D.
-func instantiate_view_model() -> Node:
-	if _descriptor.view_model_scene == null:
+## Returns null if there isn't one. Otherwise, returns an ItemViewModel2D.
+func instantiate_view_model_2d() -> ItemViewModel2D:
+	if _descriptor.view_model_2d_scene == null:
 		return null
 	
-	var new_view_model = _descriptor.view_model_scene.instantiate()
+	var new_view_model = _descriptor.view_model_2d_scene.instantiate()
 	new_view_model.setup(self)
 	return new_view_model
 
-## Places this item instance in the game world. If it was in an inventory, it is
+## Spawn and return a new instance of this item's view model, if there is one. 
+## Returns null if there isn't one. Otherwise, returns an ItemViewModel3D.
+func instantiate_view_model_3d() -> ItemViewModel3D:
+	if _descriptor.view_model_3d_scene == null:
+		return null
+	
+	var new_view_model = _descriptor.view_model_3d_scene.instantiate()
+	new_view_model.setup(self)
+	return new_view_model
+
+## Places this item instance in the 2D game world. If it was in an inventory, it is
 ## removed from that inventory.
 ## Returns
 ##	`InstanceError.OK` if the item was placed in the game world
-##	`InstanceError.ALREADY_EXISTS` if the item is already in the game world
+##	`InstanceError.ALREADY_EXISTS` if the item is already in the 2D or 3D game world
 ##	`InstanceError.SCENE_MISSING` if there is no scene in the descriptor for a
 ##		WorldItem
-func put_in_world(world_root: Node=null) -> InstanceError:
-	if _space_state == SpaceState.IN_WORLD:
+func put_in_world_2d(world_root: Node = null) -> InstanceError:
+	if _space_state == SpaceState.IN_WORLD_2D:
 		return InstanceError.ALREADY_EXISTS
-	if _descriptor.world_item_scene == null:
+	if _space_state == SpaceState.IN_WORLD_3D:
+		return InstanceError.ALREADY_EXISTS
+	if _descriptor.world_item_2d_scene == null:
 		return InstanceError.SCENE_MISSING
 	
 	# Use the window as world root, if not provided
 	if world_root == null: world_root = get_window()
-	
 	_remove_from_inventory() # Remove from an inventory, if we're in one.
-	
-	# Spawn and setup the new world item
-	var world_item = get_descriptor().world_item_scene.instantiate()
+
+	# Spawn and setup the new 2D world item
+	var world_item = get_descriptor().world_item_2d_scene.instantiate()
 	world_item.setup(self)
 	world_root.add_child(world_item)
-	_current_world_item = world_item
-	
+	_current_world_item_2d = world_item
+
 	# Reparent us to the world item to make it easier to visualize where
 	# this item is, in the editor.
-	_change_parent(_current_world_item)
+	_change_parent(_current_world_item_2d)
+
+	_space_state = SpaceState.IN_WORLD_2D
+	return InstanceError.OK
+
+## Places this item instance in the 3D game world. If it was in an inventory, it is
+## removed from that inventory.
+## Returns
+##	`InstanceError.OK` if the item was placed in the game world
+##	`InstanceError.ALREADY_EXISTS` if the item is already in the 2D or 3D game world
+##	`InstanceError.SCENE_MISSING` if there is no scene in the descriptor for a
+##		WorldItem
+func put_in_world_3d(world_root: Node = null) -> InstanceError:
+	if _space_state == SpaceState.IN_WORLD_2D:
+		return InstanceError.ALREADY_EXISTS
+	if _space_state == SpaceState.IN_WORLD_3D:
+		return InstanceError.ALREADY_EXISTS
+	if _descriptor.world_item_3d_scene == null:
+		return InstanceError.SCENE_MISSING
 	
-	_space_state = SpaceState.IN_WORLD
+	# Use the window as world root, if not provided
+	if world_root == null: world_root = get_window()
+	_remove_from_inventory() # Remove from an inventory, if we're in one.
+
+	# Spawn and setup the new 3D world item
+	var world_item = get_descriptor().world_item_3d_scene.instantiate()
+	world_item.setup(self)
+	world_root.add_child(world_item)
+	_current_world_item_3d = world_item
+
+	# Reparent us to the world item to make it easier to visualize where
+	# this item is, in the editor.
+	_change_parent(_current_world_item_3d)
+
+	_space_state = SpaceState.IN_WORLD_3D
 	return InstanceError.OK
 
 ## Places this item instance in an inventory. If it was in the world, it is
@@ -276,11 +326,23 @@ func _remove_from_inventory() -> bool:
 ##	`true` if it was removed from the world, and the WorldItem was free'd
 ##	`false` if there was no WorldItem, and we're not in the game world.
 func _remove_from_world() -> bool:
-	if _current_world_item == null:
-		return false
-		
-	_current_world_item.queue_free()
-	_current_world_item = null
+	var actually_removed = false
+	
+	# Remove our 2D world item, if there is one.
+	if  _current_world_item_2d != null:
+		_current_world_item_2d.queue_free()
+		_current_world_item_2d = null
+		actually_removed = false
+	# Remove our 3D world item, if there is one.
+	if _current_world_item_3d != null:
+		_current_world_item_3d.queue_free()
+		_current_world_item_3d = null
+		actually_removed = false
+
+	# If we didn't end up removing any world items, EXIT EARLY
+	if not actually_removed: return false
+
+	# OTHERWISE - we removed world items, so update our state properly!
 	_space_state = SpaceState.NOWHERE
 	return true
 
