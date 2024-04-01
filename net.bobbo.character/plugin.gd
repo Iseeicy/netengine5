f5bf02bdcb7784b9c5a52d2f779dec94bb605cc9
holly.fromthe.nexus <iseeicyhawkston@gmail.com>
2024-04-01 15:25:51 -0500

    Created boilerplate for char agent

--- net.bobbo.character/plugin.gd
+++ net.bobbo.character/plugin.gd
@@ -2,7 +2,9 @@
 extends EditorPlugin
 
 func _enter_tree():
-	add_custom_type("CharacterDefinition", "Resource", preload("character_definition.gd"), preload("icons/character_definition.png"))
+	add_custom_type("CharacterAgent3D", "CharacterBody3D", preload ("character_agent_3d.gd"), preload ("icons/character_definition.png"))
+	add_custom_type("CharacterDefinition", "Resource", preload ("character_definition.gd"), preload ("icons/character_definition.png"))
 
 func _exit_tree():
 	remove_custom_type("CharacterDefinition")
+	remove_custom_type("CharacterAgent3D")
