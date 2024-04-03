8cfd2a9c8de78de52a268a86e0ac6602cff1e199
holly.fromthe.nexus <iseeicyhawkston@gmail.com>
2024-04-03 16:14:49 -0500

    Put together inputs

--- net.bobbo.entity-input/player_input_action.gd
+++ net.bobbo.entity-input/player_input_action.gd
@@ -2,15 +2,10 @@
 ## Making this node based means we can allow a designer in Godot to
 ## define what Input Actions should be listened for IN EDITOR, rather
 ## than modifying code.
+## The name of this node should be the name of the Input Action to use.
+@tool
 class_name PlayerInputAction
 extends Node
-
-#
-#   Exports
-#
-
-## The name of the action to listen for, in Godot's InputMap.
-@export var action_name: String = "SET_ME"
 
 #
 #   Public Functions
@@ -22,15 +17,15 @@ func get_input_state() -> EntityInput.InputState:
 	var state := EntityInput.InputState.NONE
 
 	# If this action doesn't exist, EXIT EARLY
-	if not InputMap.has_action(action_name):
+	if not InputMap.has_action(name):
 		return state
 
 	# Manipulate the state according to actual user input
-	if Input.is_action_just_pressed(action_name):
+	if Input.is_action_just_pressed(name):
 		state = state | EntityInput.InputState.JUST_DOWN
-	if Input.is_action_pressed(action_name):
+	if Input.is_action_pressed(name):
 		state = state | EntityInput.InputState.PRESSED
-	if Input.is_action_just_released(action_name):
+	if Input.is_action_just_released(name):
 		state = state | EntityInput.InputState.JUST_UP
 
 	return state
