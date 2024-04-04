41c6f75abb852abdf6865c90f99f24e49a7b5840
holly.fromthe.nexus <iseeicyhawkston@gmail.com>
2024-04-04 15:55:52 -0500

    Added prio, and getters

--- net.bobbo.virtual-camera/virtual_camera_3d.gd
+++ net.bobbo.virtual-camera/virtual_camera_3d.gd
@@ -14,6 +14,25 @@ const GROUP_NAME: String = "vcams_3d_all"
 const VISIBLE_GROUP_NAME: String = "vcams_3d_visible"
 
 #
+#	Exports
+#
+
+## The main priority of this VCam, compared to others of the same type.
+## If there's no priority specified for a given viewport in
+## `_specific_priorities`, then this value is used. A higher value will
+## cause this VCam to overtake VCams of a lower priority value.
+@export var main_priority: int = 0
+
+#
+#	Private Variables
+#
+
+## Dicitonary[RID, int]
+## The specific priority of this camera, in a given viewport by it's
+## resource ID.
+var _specific_priorities: Dictionary = {}
+
+#
 #	Static Functions
 #
 
@@ -43,6 +62,46 @@ static func get_visible(scene_tree: SceneTree) -> Array[VirtualCamera3D]:
 	var found_vcams: Array[VirtualCamera3D] = []
 	found_vcams.assign(scene_tree.get_nodes_in_group(VISIBLE_GROUP_NAME))
 	return found_vcams
+
+
+#
+#	Public Functions
+#
+
+
+## Get the priority of this VCam, relative to the viewport.
+## Args:
+##	`viewport`: The Viewport that we want to use as reference when
+##		getting this VCam's priority.
+## Returns:
+##	`int` `main_priority`, if `viewport` is null.
+##	`int` `main_priority`, if there is no specific priority for the
+##		given viewport.
+##	`int` The specific priority for the given viewport, if there was
+##		one assigned.
+func get_priority(viewport: Viewport) -> int:
+	return get_priority_by_rid(
+		viewport.get_viewport_rid() if viewport else null
+	)
+
+
+## Get the priority of this VCam, relative to the viewport using the
+## RID of the viewport as reference.
+## Args:
+##	`viewport_rid`: The resource ID of the Viewport that we want to use
+##		as reference when getting this VCam's priority.
+## Returns:
+##	`int` `main_priority`, if `viewport_rid` is null.
+##	`int` `main_priority`, if there is no specific priority for the
+##		given viewport.
+##	`int` The specific priority for the given viewport, if there was
+##		one assigned.
+func get_priority_by_rid(viewport_rid: RID) -> int:
+	# If we were not given a proper RID, just return the main priority.
+	if viewport_rid == null:
+		return main_priority
+
+	return _specific_priorities.get(viewport_rid, main_priority)
 
 
 #
