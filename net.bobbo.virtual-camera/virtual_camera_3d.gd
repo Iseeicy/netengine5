4390d9be31b5a625d2a3cb37d768cd68941bd04c
holly.fromthe.nexus <iseeicyhawkston@gmail.com>
2024-04-04 15:11:39 -0500

    Adds to group

--- net.bobbo.virtual-camera/virtual_camera_3d.gd
+++ net.bobbo.virtual-camera/virtual_camera_3d.gd
@@ -13,4 +13,28 @@ const GROUP_NAME: String = "vcams_3d_all"
 ## the `visible` property.
 const VISIBLE_GROUP_NAME: String = "vcams_3d_visible"
 
-## TODO
+#
+#   Godot Functions
+#
+
+
+func _init() -> void:
+	# Make sure we react to visibility changes
+	visibility_changed.connect(_on_self_visibility_changed.bind())
+
+
+func _enter_tree() -> void:
+	add_to_group(GROUP_NAME)
+
+
+func _exit_tree() -> void:
+	remove_from_group(GROUP_NAME)
+
+
+#
+#   Signals
+#
+
+
+func _on_self_visibility_changed() -> void:
+	return
