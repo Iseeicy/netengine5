42c96227349fe15ea32352f868b8e7e595dce16d
holly.fromthe.nexus <iseeicyhawkston@gmail.com>
2024-04-04 15:08:01 -0500

    Constants

--- net.bobbo.virtual-camera/virtual_camera_3d.gd
+++ net.bobbo.virtual-camera/virtual_camera_3d.gd
@@ -1,4 +1,16 @@
 class_name VirtualCamera3D
 extends Node3D
 
+#
+#   Constants
+#
+
+## The name of the group that ALL `VirtualCamera3D`s are stored in.
+const GROUP_NAME: String = "vcams_3d_all"
+
+## The name of the group that all VISIBLE `VirtualCamera3D`s are stored
+## in. VCams will be added and removed to this group upon change in
+## the `visible` property.
+const VISIBLE_GROUP_NAME: String = "vcams_3d_visible"
+
 ## TODO
