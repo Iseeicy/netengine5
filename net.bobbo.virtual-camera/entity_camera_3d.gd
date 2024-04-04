cf2137adfaf3c1f15739113f489e18584d0563ea
holly.fromthe.nexus <iseeicyhawkston@gmail.com>
2024-04-04 16:31:54 -0500

    ECams snap to highest prio position

--- net.bobbo.virtual-camera/entity_camera_3d.gd
+++ net.bobbo.virtual-camera/entity_camera_3d.gd
@@ -1,4 +1,41 @@
 class_name EntityCamera3D
 extends Node3D
 
-## TODO
+#
+#   Godot Functions
+#
+
+
+func _process(_delta):
+	var target_camera = _find_target_camera()
+	if target_camera == null:
+		return
+
+	# Snap to the target camera
+	global_position = target_camera.global_position
+	global_rotation = target_camera.global_rotation
+
+
+#
+#   Private Functions
+#
+
+
+## Find the VCam with the highest priority in the tree.
+func _find_target_camera() -> VirtualCamera3D:
+	# Find all visible cameras. If there are none, EXIT EARLY.
+	var visible_cameras := VirtualCamera3D.get_visible(get_tree())
+	if visible_cameras.size() == 0:
+		return null
+
+	# Find the camera with the highest priority.
+	# TODO - what do we do when two cameras have the same priority?
+	var candidate_camera: VirtualCamera3D = visible_cameras[0]
+	for cam in visible_cameras:
+		if (
+			cam.get_priority(get_viewport())
+			> candidate_camera.get_priority(get_viewport())
+		):
+			candidate_camera = cam
+
+	return candidate_camera
