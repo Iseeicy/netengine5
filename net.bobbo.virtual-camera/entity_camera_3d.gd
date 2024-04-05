70c6298aa54ecafbceb1db47ba7145e18867e9bb
holly.fromthe.nexus <iseeicyhawkston@gmail.com>
2024-04-05 11:50:16 -0500

    `EntityCamera`: Smooth Transitions (#98)
    Implements transitions for ecams when cutting between vcams. These use the easing types from easing.net ! Whee!!

--- net.bobbo.virtual-camera/entity_camera_3d.gd
+++ net.bobbo.virtual-camera/entity_camera_3d.gd
@@ -2,18 +2,70 @@ class_name EntityCamera3D
 extends Node3D
 
 #
+#	Exports
+#
+
+## The transition to use when cutting between two different vcams
+@export var default_transition: CameraTransition = null
+
+#
+#	Private Variables
+#
+
+## Keeps track of the active camera transition happening, if there is
+## any.
+var _current_transitioner: CameraTransitioner3D = null
+
+## The last known vcam to have the highest priority.
+var _target_camera: VirtualCamera3D = null
+
+#
 #   Godot Functions
 #
 
 
-func _process(_delta):
-	var target_camera = _find_target_camera()
-	if target_camera == null:
-		return
+func _ready():
+	if default_transition == null:
+		default_transition = CameraTransition.new()
 
-	# Snap to the target camera
-	global_position = target_camera.global_position
-	global_rotation = target_camera.global_rotation
+
+func _process(_delta):
+	var current_target_camera = _find_target_camera()
+
+	# If we have a new target camera that is NOT the one we previously
+	# knew about...
+	if current_target_camera != _target_camera:
+		# ...update our known target camera
+		_target_camera = current_target_camera
+
+		# If we were already transitioning, stop the transition
+		if _current_transitioner:
+			_current_transitioner.queue_free()
+			_current_transitioner = null
+
+		# If the new target camera is real, start a transition
+		# TODO - We should support allowing cameras to specify
+		# transitions to other specific cameras.
+		if current_target_camera != null:
+			# Create the transitioner
+			var new_transitioner = default_transition.create_transitioner_3d(
+				self, current_target_camera
+			)
+			_current_transitioner = new_transitioner
+
+			# Setup a callback to remove the transitioner val upon finishing
+			# transition
+			var on_complete_func = func():
+				if _current_transitioner == new_transitioner:
+					_current_transitioner = null
+			new_transitioner.transition_complete.connect(on_complete_func.bind())
+	
+	# If we have a target camera and we're not still transitioning to it...
+	elif _target_camera != null and _current_transitioner == null:
+		# ...then snap our position!
+		global_position = _target_camera.global_position
+		global_rotation = _target_camera.global_rotation
+
 
 
 #
