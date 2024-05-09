e96bfcdc92f5683ffd4e736557ac69b1ad65efd7
holly.fromthe.nexus <iseeicyhawkston@gmail.com>
2024-05-09 14:32:07 -0500

    Created stubs for public funcs

--- net.bobbo.entity-input/simulated_input.gd
+++ net.bobbo.entity-input/simulated_input.gd
@@ -20,3 +20,26 @@ func get_local_movement_dir() -> Vector3:
 	super()
 	# TODO
 	return Vector3.ZERO
+
+
+#
+#   Public Functions
+#
+
+
+## Simulate quickly pressing a button and then releasing it. Marks the input as
+## down, and then up on the next frame.
+## Args:
+##  `action_name`: The name of the Input Action to simulate.
+func simulate_action_oneshot(action_name: String) -> void:
+	# TODO
+	return
+
+
+## Simulate pressing a button down or releasing a button.
+## Args:
+##  `action_name`: The name of the Input Action to simulate.
+##  `is_down`: Should we simulate the button being held down? Or released?
+func simulate_action(action_name: String, is_down: bool) -> void:
+	# TODO
+	return
