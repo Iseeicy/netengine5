f57c7c33d750bd07f0e5912700c26c3786d81215
holly.fromthe.nexus <iseeicyhawkston@gmail.com>
2024-07-30 14:51:53 -0500

    Spacing

--- net.bobbo.state-machine/bobbo_state.gd
+++ net.bobbo.state-machine/bobbo_state.gd
@@ -10,39 +10,50 @@ var _is_active: bool = true
 #	Public Functions
 #
 
+
 func state_enter(_message: Dictionary = {}) -> void:
 	return
-	
+
+
 func state_unhandled_input(_event: InputEvent) -> void:
 	return
-	
+
+
 func state_process(_delta: float) -> void:
 	return
-	
+
+
 func state_physics_process(_delta: float) -> void:
 	return
-	
+
+
 func state_exit() -> void:
 	return
 
+
 func get_state_path() -> String:
 	return _state_path
+
 
 func set_is_active(should_be_active: bool) -> void:
 	_is_active = should_be_active
 	set_block_signals(!_is_active)
 
+
 func get_is_active() -> bool:
 	return _is_active
+
 
 #
 #	Private Functions
 #
 
+
 # Get the state from our parent, or null if there is none
 func _get_parent_state() -> BobboState:
 	# If the parent is not a player state, this will be null
 	return get_parent() as BobboState
+
 
 # Recursively climb the tree to get the state machine that
 # we belong to
@@ -52,8 +63,9 @@ func _get_state_machine(node: Node) -> Node:
 	else:
 		return _get_state_machine(node.get_parent())
 
+
 func _get_state_path(node: Node) -> String:
-	if node == null or  node is BobboStateMachine:
+	if node == null or node is BobboStateMachine:
 		return ""
 	else:
 		var rest_of_path = _get_state_path(node.get_parent())
