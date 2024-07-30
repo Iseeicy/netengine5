d5543b3ca376745b88060117e3b7524bed5c34b0
holly.fromthe.nexus <iseeicyhawkston@gmail.com>
2024-07-30 15:53:01 -0500

    Go to position routine is cleaner

--- net.bobbo.npc/states/go_to_position.gd
+++ net.bobbo.npc/states/go_to_position.gd
@@ -2,11 +2,14 @@ class_name NPCStateGoToPosition
 extends NPCState
 
 #
-#	Exports
+#	Constants
 #
 
-## Invoked when the NPC has reached its target position.
-signal path_completed
+const CANCEL_REASON_PATH_FAILED := "path_failed"
+
+#
+#	Exports
+#
 
 ## The global position to path to.
 @export var target_position: Vector3 = Vector3.ZERO
@@ -18,7 +21,6 @@ signal path_completed
 #	Private Variables
 #
 
-var _runtime_callbacks: Array[Callable] = []
 var _actively_pathing := false
 
 #
@@ -30,38 +32,30 @@ func state_enter(message: Dictionary = {}) -> void:
 	# Accept args from the message param
 	target_position = message.get("target_position", target_position)
 	how_close = message.get("how_close", how_close)
-	if message.has("on_complete"):
-		var on_complete_func := message.get("on_complete") as Callable
-		_runtime_callbacks.append(on_complete_func)
-		path_completed.connect(on_complete_func)
 
 	agent_3d.nav_agent.target_desired_distance = how_close
 	agent_3d.nav_agent.target_position = target_position
 	_actively_pathing = true
 
 
-func state_exit() -> void:
-	# Disconnect any callbacks created at runtime
-	for callback in _runtime_callbacks:
-		path_completed.disconnect(callback)
-
-
 func state_physics_process(_delta: float) -> void:
-	# If our navigation isn't finished and we're not as close as we wanna be...
-	if (
-		not agent_3d.nav_agent.is_navigation_finished()
-		and agent_3d.nav_agent.distance_to_target() > how_close
-	):
-		var next_pos = agent_3d.move_towards_nav_target()
+	if not _actively_pathing:
+		return
 
-		# Face the path
-		var look_pos = next_pos
-		look_pos.y = agent_3d.head_node.global_position.y
-		agent_3d.look_at_point(look_pos)
-	else:
-		# Face the target
-		agent_3d.look_at_point(target_position)
-
-	if agent_3d.nav_agent.is_navigation_finished() and _actively_pathing:
+	# If we're pathing and we're now within range of the target...
+	if agent_3d.nav_agent.distance_to_target() <= how_close:
+		# Mark that we should no longer be pathing
 		_actively_pathing = false
-		path_completed.emit()
+
+		# Face the target, and mark that this routine is complete
+		agent_3d.look_at_point(target_position)
+		completed.emit()
+		return
+
+	# Path towards the target
+	var next_pos = agent_3d.move_towards_nav_target()
+
+	# Face the path
+	var look_pos = next_pos
+	look_pos.y = agent_3d.head_node.global_position.y
+	agent_3d.look_at_point(look_pos)
