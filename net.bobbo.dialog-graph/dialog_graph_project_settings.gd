32b8d35aa7c4a3d641ba906c18549bc24eaa2b7c
holly.fromthe.nexus <iseeicyhawkston@gmail.com>
2024-08-22 15:04:42 -0500

    Added docs to DialogGraphProjectSettings

--- net.bobbo.dialog-graph/dialog_graph_project_settings.gd
+++ net.bobbo.dialog-graph/dialog_graph_project_settings.gd
@@ -7,8 +7,11 @@ extends RefCounted
 #   Constants
 #
 
+## The ProjectSettings key of the DialogGraphNodeDescriptor Array for
+## GraphNodeDB to use.
 const DESCRIPTORS_SETTINGS_KEY := "netengine5/dialog_graph/descriptors"
 
+## The default value of the setting at DESCRIPTORS_SETTINGS_KEY
 const DEFAULT_DESCRIPTORS := [
 	preload("nodes/entry/entry_desc.tres").resource_path,
 	preload("nodes/dialog_text/dialog_text_desc.tres").resource_path,
@@ -17,12 +20,47 @@ const DEFAULT_DESCRIPTORS := [
 ]
 
 #
-#   Static Functions
+#   Public Functions
 #
 
 
+## Setup `net.bobbo.dialog-graph`'s ProjectSettings. Intended to only be called
+## internally by this plugin.
 static func internal_setup() -> void:
-	# Setup the DialogGraphNodeDescriptor field
+	_setup_descriptors_property()
+	ProjectSettings.save()
+
+
+## Get the DialogGraphNodeDescriptors identified in ProjectSettings.
+static func get_descriptors() -> Array[DialogGraphNodeDescriptor]:
+	var descriptor_paths: Array = ProjectSettings.get_setting(
+		DESCRIPTORS_SETTINGS_KEY, DEFAULT_DESCRIPTORS
+	)
+
+	# Load all DialogGraphNodeDescriptors from our settings
+	var results: Array[DialogGraphNodeDescriptor] = []
+	for path in descriptor_paths:
+		# Sometimes a path can be an empty string - skip if that's the case.
+		if not path:
+			continue
+
+		# Load the resource at the given path, and add it to our results if the
+		# resource is actually a DialogGraphNodeDescriptor
+		var loaded_resource = load(path)
+		if loaded_resource is DialogGraphNodeDescriptor:
+			results.append(loaded_resource)
+	return results
+
+
+#
+#	Private Functions
+#
+
+
+## Setup the settings for DESCRIPTORS_SETTINGS_KEY in ProjectSettings. This
+## will ensure that the property is displayed correctly in the GUI, and ensure
+## that a default value is set correctly.
+static func _setup_descriptors_property() -> void:
 	ProjectSettings.add_property_info(
 		{
 			name = DESCRIPTORS_SETTINGS_KEY,
@@ -41,24 +79,3 @@ static func internal_setup() -> void:
 		ProjectSettings.set_setting(
 			DESCRIPTORS_SETTINGS_KEY, DEFAULT_DESCRIPTORS
 		)
-
-	# Save our changes
-	ProjectSettings.save()
-
-
-static func get_descriptors() -> Array[DialogGraphNodeDescriptor]:
-	var descriptor_paths: Array = ProjectSettings.get_setting(
-		DESCRIPTORS_SETTINGS_KEY, DEFAULT_DESCRIPTORS
-	)
-
-	# Load all DialogGraphNodeDescriptors from our settings
-	var descriptors: Array[DialogGraphNodeDescriptor] = []
-	for path in descriptor_paths:
-		if not path:
-			continue
-
-		var loaded_resource = load(path)
-		if loaded_resource is DialogGraphNodeDescriptor:
-			descriptors.append(loaded_resource)
-
-	return descriptors
