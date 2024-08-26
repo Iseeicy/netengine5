c826ba1d38550fb5a25e9f989d603bdf7221a66a
holly.fromthe.nexus <iseeicyhawkston@gmail.com>
2024-08-26 15:39:04 -0500

    Renamed PlayerModel to PlayerModelRenderer3D

--- net.bobbo.player-model/plugin.gd
+++ net.bobbo.player-model/plugin.gd
@@ -1,13 +1,15 @@
 @tool
 extends EditorPlugin
 
+
 func _enter_tree():
 	add_custom_type(
-		"PlayerModel",						# Type name
-		"Node3D", 							# Base type name
-		preload("player_model.gd"), 		# Script for type
-		preload("icons/player_model.png")	# Icon for type
+		"PlayerModelRenderer3D",  # Type name
+		"Node3D",  # Base type name
+		preload("player_model_renderer_3d.gd"),  # Script for type
+		preload("icons/player_model.png")  # Icon for type
 	)
 
+
 func _exit_tree():
-	remove_custom_type("PlayerModel")
+	remove_custom_type("PlayerModelRenderer3D")
