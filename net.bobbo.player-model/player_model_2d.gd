ac67c5255c8b5b2b7524910088c712ee9bd6276f
holly.fromthe.nexus <iseeicyhawkston@gmail.com>
2024-10-04 11:22:23 -0500

    Better PlayerModel docs

--- net.bobbo.player-model/player_model_2d.gd
+++ net.bobbo.player-model/player_model_2d.gd
@@ -1,3 +1,6 @@
+## A 2D PlayerModel. This should be the root for any 2D PlayerModel
+## scene. Controls the visual and animation state of a playermodel.
+## Intended to be used and instantiated by PlayerModelRenderer2D.
 class_name PlayerModel2D
 extends Node2D
 
@@ -13,21 +16,31 @@ extends Node2D
 #
 
 
+## Not intended to be called directly. Call through
+## `PlayerModelRenderer2D.set_anim_param()`.
 func set_anim_param(param: String, value: Variant) -> void:
 	PlayerModelAnimParams.set_anim_param(animation_tree, param, value)
 
 
+## Not intended to be called directly. Call through
+## `PlayerModelRenderer2D.set_main_state()`.
 func set_main_state(value: String) -> void:
 	PlayerModelAnimParams.set_main_state(animation_tree, value)
 
 
+## Not intended to be called directly. Call through
+## `PlayerModelRenderer2D.set_movement_state()`.
 func set_movement_state(value: String) -> void:
 	PlayerModelAnimParams.set_movement_state(animation_tree, value)
 
 
+## Not intended to be called directly. Call through
+## `PlayerModelRenderer2D.set_movement_vector()`.
 func set_movement_vector(input_movement: Vector2) -> void:
 	PlayerModelAnimParams.set_movement_vector(animation_tree, input_movement)
 
 
+## Not intended to be called directly. Call through
+## `PlayerModelRenderer2D.fire_oneshot()`.
 func fire_oneshot(param: String) -> void:
 	PlayerModelAnimParams.fire_oneshot(animation_tree, param)
